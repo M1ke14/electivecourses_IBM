7e95d3d0d56fb9007996e1f4962fae33f9be6efb
MariaF03055 <maria.fintineanu04@e-uvt.ro>
2024-04-04 20:16:45 +0300

    First commit: testing spring boot

---
+++ src/test/java/com/university/elecitvecoursesappmanagement/ElecitveCoursesAppManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.elecitvecoursesappmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecitveCoursesAppManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
