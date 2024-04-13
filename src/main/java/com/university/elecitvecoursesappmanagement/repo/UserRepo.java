ba47d06a741a91aac9c0ec696adc476d494dd803
MariaF03055 <maria.fintineanu04@e-uvt.ro>
2024-04-13 17:40:27 +0300

    Created the User table

---
+++ src/main/java/com/university/elecitvecoursesappmanagement/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.university.elecitvecoursesappmanagement.repo;
+/*
+import com.university.elecitvecoursesappmanagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
+*/
